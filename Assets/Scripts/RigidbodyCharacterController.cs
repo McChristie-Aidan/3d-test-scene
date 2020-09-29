29d34987fa160241af48ead5d4a6e2bc617ff2dc
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-09-29 18:10:07 -0500

    adds Physics materials

---
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationForce = 10;
+    [SerializeField]
+    private float maxSpeed = 2;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDir = new Vector3(input.x, 0, input.y);
+
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(inputDir * accelerationForce, ForceMode.Acceleration);
+        }
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+}
