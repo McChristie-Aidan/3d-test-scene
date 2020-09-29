c068c5e989d294df4f2f948348207158d95ace02
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-09-29 18:23:23 -0500

    added more physics materials and adjusted the numbers

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -8,18 +8,31 @@ public class RigidbodyCharacterController : MonoBehaviour
     private float accelerationForce = 10;
     [SerializeField]
     private float maxSpeed = 2;
+    [SerializeField]
+    private PhysicMaterial stoppingMat, movingMat;
 
     private new Rigidbody rigidbody;
     private Vector2 input;
+    private new CapsuleCollider collider;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
     }
 
     private void FixedUpdate()
     {
         var inputDir = new Vector3(input.x, 0, input.y);
+
+        if (inputDir.magnitude > 0)
+        {
+            collider.material = movingMat;
+        }
+        else
+        {
+            collider.material = stoppingMat;
+        }
 
         if (rigidbody.velocity.magnitude < maxSpeed)
         {
