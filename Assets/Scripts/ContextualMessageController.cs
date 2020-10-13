975b637e3d8bd7edfe87c674174e499b6d32fa3a
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-10-12 22:14:08 -0500

    adds working contextual message triggers

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -14,8 +14,6 @@ public class ContextualMessageController : MonoBehaviour
         messageText = GetComponent<TMP_Text>();
 
         canvasGroup.alpha = 0;
-
-        StartCoroutine(ShowMessage("Testing", 2));
     }
 
     private IEnumerator ShowMessage(string message, float duration)
@@ -24,5 +22,20 @@ public class ContextualMessageController : MonoBehaviour
         messageText.text = message;
         yield return new WaitForSeconds(duration);
         canvasGroup.alpha = 0;
+    }
+
+    private void OnContextualMessageTriggered()
+    {
+        StartCoroutine(ShowMessage("Testing", 2));
+    }
+
+    private void OnEnable()
+    {
+        ContexualMessageTrigger.ContextualMessageTriggered += OnContextualMessageTriggered;
+    }
+
+    private void OnDisable()
+    {
+        ContexualMessageTrigger.ContextualMessageTriggered -= OnContextualMessageTriggered;
     }
 }
